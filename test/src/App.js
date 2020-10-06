a1db28fa220b22c0599f8fd8f54f3f0579524ad1
Mary Wichmann <mary.wichmann@sogeti.com>
2020-10-06 14:50:44 +0200

    increment button

--- test/src/App.js
+++ test/src/App.js
@@ -2,23 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Button(props) {
+
+  const handleClick = () => props.onClickFunction(props.increment);
+
+  return (
+    <button className="Button" onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display(props) {
+  return(
+    <div className="Message">{props.message}</div>
+  );
+}
+
 function App() {
+
+  const [counter, setCounter] = React.useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <br/>
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <br/>
+      <Button onClickFunction={incrementCounter} increment={5}/>
+      <br/>
+      <Button onClickFunction={incrementCounter} increment={10}/>
+      <br/>
+      <br/>
+      <Display message={counter}/>
     </div>
   );
 }
