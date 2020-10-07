6154d46ed6ae3eb0cd94e2ca130428cbac424b6a
Mary Wichmann <mary.wichmann@sogeti.com>
2020-10-07 14:39:26 +0200

    unsuccesfull test of radiobuttons

--- test/src/App.js
+++ test/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import fire from './images/fire.svg';
+import earth from './images/earth.svg';
+import water from './images/water.svg';
+import air from './images/air.svg';
 import './App.css';
 
 
@@ -15,28 +18,104 @@ function Button(props) {
 }
 
 function Display(props) {
-  return(
+  return (
     <div className="Message">{props.message}</div>
   );
 }
 
+
+function Form() {
+
+  const [element, setElement] = React.useState('fire');
+  const handleOptionChange = (value) => setElement(value);
+  
+  return (
+    <form className="Form">
+      <div className="Form-elements">
+        <div className="Form-radio">
+          <label>
+            <img src={fire} alt="fire" />
+            <br />
+              fire
+              <input
+              type="radio"
+              value="fire"
+              checked={element === 'fire'}
+              onChange={handleOptionChange}
+            />
+          </label>
+        </div>
+        <div className="Form-radio">
+          <label>
+            <img src={earth} alt="earth" />
+            <br />
+              earth
+              <input
+              type="radio"
+              value="earth"
+              checked={element === 'earth'}
+              onChange={handleOptionChange}
+            />
+          </label>
+        </div>
+        <div className="Form-radio">
+          <label>
+            <img src={water} alt="water" />
+            <br />
+              water
+              <input
+              type="radio"
+              value="water"
+              checked={element === 'water'}
+              onChange={handleOptionChange}
+            />
+          </label>
+        </div>
+        <div className="Form-radio">
+          <label>
+            <img src={air} alt="air" />
+            <br />
+              air
+              <input
+              type="radio"
+              value="air"
+              checked={element === 'air'}
+              onChange={handleOptionChange}
+            />
+          </label>
+        </div>
+      </div>
+
+      {/* <Button onClickFunction={incrementCounter} increment={1} />
+        <Button onClickFunction={incrementCounter} increment={5} />
+        <Button onClickFunction={saveForm} increment={10} /> */}
+    </form>
+  );
+}
+
+
 function App() {
 
-  const [counter, setCounter] = React.useState(0);
-  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+
 
   return (
-    <div className="App">
-      <br/>
-      <br/>
-      <Button onClickFunction={incrementCounter} increment={1}/>
-      <br/>
-      <Button onClickFunction={incrementCounter} increment={5}/>
-      <br/>
-      <Button onClickFunction={incrementCounter} increment={10}/>
-      <br/>
-      <br/>
-      <Display message={counter}/>
+    <div className="Wrapper">
+      <header className="Header">
+
+      </header>
+      <div className="Body">
+        <Form />
+
+
+
+
+        <div className="Result">
+          <Display message={0} />
+        </div>
+      </div>
+      <footer className="Footer">
+
+      </footer>
     </div>
   );
 }
