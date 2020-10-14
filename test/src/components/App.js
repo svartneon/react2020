e58a7bbde2a7a1ff9456e8e5410a2d0239d2fb78
Mary Wichmann <mary.wichmann@sogeti.com>
2020-10-14 13:36:00 +0200

    new dropdown component, added useref useeffect

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -1,44 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from './Form.js';
+import Footer from './Footer.js';
+import { monsters } from './monsters.js';
 import './../styles/App.css';
-import fire_female from './../images/fire_female.svg';
-import fire_male from './../images/fire_male.svg';
-import earth_female from './../images/earth_female.svg';
-import earth_male from './../images/earth_male.svg';
-import water_female from './../images/water_female.svg';
-import water_male from './../images/water_male.svg';
-import air_female from './../images/air_female.svg';
-import air_male from './../images/air_male.svg';
 
 function App() {
 
-  const [element, setElement] = React.useState('Fire');
-  const [result, setResult] = React.useState('');
-  const [gender, setGender] = React.useState("Female");
-  const [name, setName] = React.useState("Nameless");
+  const [element, setElement] = useState('Fire');
+  const [result, setResult] = useState('');
+  const [gender, setGender] = useState("Female");
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
   const handleElementChange = (event) => setElement(event.target.value);
   const handleGenderChange = (event) => setGender(event.target.value);
   const handleNameChange = (event) => setName(event.target.value);
 
-  React.useEffect(() => {
-    document.title = `${element} Monster`;
-  });
+  useEffect(() => {
+    document.title = count + " monsters generated";
+  }, [count]);
+
+  const monsterList = monsters();
+    
 
   function handleSubmit(event) {
+    setCount(count+1);
     event.preventDefault();
 
-    for (const key in monsters) {
-      if (element === monsters[key].element && gender === monsters[key].gender) {
+    for (let key in monsterList) {
+      if (element === monsterList[key].element && gender === monsterList[key].gender) {
         setResult(
           <div>
-            {monsters[key].image}
+            {monsterList[key].image}
             <p>
-              {name}{monsters[key].type}
+              {name}{monsterList[key].type}
               <br />
               <br />
-              damage type: {monsters[key].element}
+              damage type: {monsterList[key].element}
               <br />
-              gender: {monsters[key].gender}
+              gender: {monsterList[key].gender}
               <br />
               damage: {Math.floor(Math.random() * (4 - 1) + 1)}-{Math.round(Math.random() * (10 - 4) + 4)}
             </p>
@@ -51,57 +50,6 @@ function App() {
       }
     }
   }
-
-  const monsters = [
-    {
-      "element": "Fire",
-      "gender": "Female",
-      "image": <img src={fire_female} alt="result" />,
-      "type": " the viscious lava octopus."
-    },
-    {
-      "element": "Fire",
-      "gender": "Male",
-      "image": <img src={fire_male} alt="result" />,
-      "type": " the scolding fire dragon."
-    },
-    {
-      "element": "Earth",
-      "gender": "Female",
-      "image": <img src={earth_female} alt="result" />,
-      "type": " the sharptooth squirrel."
-    },
-    {
-      "element": "Earth",
-      "gender": "Male",
-      "image": <img src={earth_male} alt="result" />,
-      "type": " the stinky orch."
-    },
-    {
-      "element": "Water",
-      "gender": "Female",
-      "image": <img src={water_female} alt="result" />,
-      "type": " the traitorous mermaid."
-    },
-    {
-      "element": "Water",
-      "gender": "Male",
-      "image": <img src={water_male} alt="result" />,
-      "type": " bigfoot's baby brother."
-    },
-    {
-      "element": "Air",
-      "gender": "Female",
-      "image": <img src={air_female} alt="result" />,
-      "type": " the tempestuous pixie."
-    },
-    {
-      "element": "Air",
-      "gender": "Male",
-      "image": <img src={air_male} alt="result" />,
-      "type": " the judgemental angel."
-    }
-  ];
 
   return (
     <div className="Wrapper">
@@ -119,15 +67,7 @@ function App() {
           {result}
         </div>
       </div>
-      <footer className="Footer">
-        Images by  
-        <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=147719">OpenClipart-Vectors</a>,
-        <a href="https://pixabay.com/users/endresz-3286579/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1663243">Mária Endrész</a>,
-        <a href="https://pixabay.com/users/pandannaimagen-10193585/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4378680">Pandanna Imagen</a>,
-        <a href="https://pixabay.com/users/gorkhs-268986/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3757569">Gorkhs</a> and 
-        <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=40860">Clker-Free-Vector-Images</a>
-        from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=147719">Pixabay</a>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ test/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <footer className="Footer">
+        <br />
+        <br />
+        <p>Images by&nbsp;
+        <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=147719">OpenClipart-Vectors</a>,&nbsp;
+        <a href="https://pixabay.com/users/endresz-3286579/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1663243">Mária Endrész</a>,&nbsp;
+        <a href="https://pixabay.com/users/pandannaimagen-10193585/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4378680">Pandanna Imagen</a>,&nbsp;
+        <a href="https://pixabay.com/users/gorkhs-268986/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3757569">Gorkhs</a> and&nbsp;
+        <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=40860">Clker-Free-Vector-Images</a> from&nbsp;
+        <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=147719">Pixabay</a>
+        </p>
+      </footer>
+    );
+}
+
+export default Footer;
