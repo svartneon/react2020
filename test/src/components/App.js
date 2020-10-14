b9ce9edd3906cab58fbd89de283a7a2735f0aaf0
Mary Wichmann <mary.wichmann@sogeti.com>
2020-10-14 15:06:21 +0200

    turned the result result into a monster component

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import Form from './Form.js';
 import Footer from './Footer.js';
+import Monster from './Monster.js';
 import { monsters } from './monsters.js';
 import './../styles/App.css';
 
 function App() {
 
   const [element, setElement] = useState('Fire');
-  const [result, setResult] = useState('');
   const [gender, setGender] = useState("Female");
   const [name, setName] = useState("");
   const [count, setCount] = useState(0);
+  const [monster, setMonster] = useState({ name: "", image: "", element: "", gender: "", type: "", damage: "" });
   const handleElementChange = (event) => setElement(event.target.value);
   const handleGenderChange = (event) => setGender(event.target.value);
   const handleNameChange = (event) => setName(event.target.value);
@@ -20,33 +21,28 @@ function App() {
   }, [count]);
 
   const monsterList = monsters();
-    
+
 
   function handleSubmit(event) {
-    setCount(count+1);
     event.preventDefault();
+    setCount(count + 1);
 
     for (let key in monsterList) {
       if (element === monsterList[key].element && gender === monsterList[key].gender) {
-        setResult(
-          <div>
-            {monsterList[key].image}
-            <p>
-              {name}{monsterList[key].type}
-              <br />
-              <br />
-              damage type: {monsterList[key].element}
-              <br />
-              gender: {monsterList[key].gender}
-              <br />
-              damage: {Math.floor(Math.random() * (4 - 1) + 1)}-{Math.round(Math.random() * (10 - 4) + 4)}
-            </p>
-          </div>
+        setMonster({
+          ...monster,
+          name: name,
+          image: monsterList[key].image,
+          element: "element: " + monsterList[key].element,
+          gender: "gender: " + monsterList[key].gender,
+          type: monsterList[key].type,
+          damage: "damage:" + Math.floor(Math.random() * (4 - 1) + 1) + "-" + Math.round(Math.random() * (10 - 4) + 4)
+        }
         );
         break;
       }
       else {
-        setResult(<p>Can't find a result</p>);
+        setMonster({ name: "", image: "Can't find monster", element: "", gender: "", type: "", damage: "" });
       }
     }
   }
@@ -64,7 +60,7 @@ function App() {
           name={name} nameHandler={handleNameChange}
         />
         <div className="Result">
-          {result}
+          <Monster monster={monster} />
         </div>
       </div>
       <Footer />
